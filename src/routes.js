7cd0c39bdd606a39c5cea93798018ee6f0ef90f5
Thiago9220_github <Thiago.ramos356@gmail.com>
2020-12-07 20:45:58 -0300

    subiu dnv

--- src/routes.js
+++ src/routes.js
@@ -10,21 +10,21 @@ routes.get('/', function (req,res){
 })
 
 // CRUD Funcionario
-routes.get('/funcionario/:id', AdminController.show);
+routes.get('/funcionario', AdminController.show);
 routes.post('/funcionario', AdminController.store);
 
 routes.put('/funcionario/:id', AdminController.update);
 routes.delete('/funcionario/:id', AdminController.delete);
 
 // CRUD Produto
-routes.get('/produto/:id', ProdutoController.show);
+routes.get('/produto', ProdutoController.show);
 routes.post('/produto', ProdutoController.store);
 
 routes.put('/produto/:id', ProdutoController.update);
 routes.delete('/produto/:id', ProdutoController.delete);
 
 // CRUD Cliente
-routes.get('/Cliente/:id', ClienteController.show);
+routes.get('/Cliente', ClienteController.show);
 routes.post('/cliente', ClienteController.store);
 
 routes.put('/cliente/:id', ClienteController.update);
