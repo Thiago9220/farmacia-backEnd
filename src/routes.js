76257d7de73d3afae52abe4f9bf615af66aa2c2d
Thiago9220_github <Thiago.ramos356@gmail.com>
2020-12-07 20:55:39 -0300

    alooraa

--- src/routes.js
+++ src/routes.js
@@ -10,21 +10,21 @@ routes.get('/', function (req,res){
 })
 
 // CRUD Funcionario
-routes.get('/funcionario', AdminController.show);
+routes.get('/funcionario/:id', AdminController.show);
 routes.post('/funcionario', AdminController.store);
 
 routes.put('/funcionario/:id', AdminController.update);
 routes.delete('/funcionario/:id', AdminController.delete);
 
 // CRUD Produto
-routes.get('/produto', ProdutoController.show);
+routes.get('/produto/:id', ProdutoController.show);
 routes.post('/produto', ProdutoController.store);
 
 routes.put('/produto/:id', ProdutoController.update);
 routes.delete('/produto/:id', ProdutoController.delete);
 
 // CRUD Cliente
-routes.get('/Cliente', ClienteController.show);
+routes.get('/cliente/:id', ClienteController.show);
 routes.post('/cliente', ClienteController.store);
 
 routes.put('/cliente/:id', ClienteController.update);
