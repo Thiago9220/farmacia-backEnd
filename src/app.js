54570bc3bbfdd46568db77aa580e82d77a063c6c
Thiago9220_github <Thiago.ramos356@gmail.com>
2020-12-07 19:40:32 -0300

    Consegui subir no git

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require("cors");
 
 require('./database');
 
@@ -17,6 +18,23 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  cors() {
+    this.server.use((req, res, next) => {
+
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type,  Accept,  Authorization"
+      );
+
+      this.server.use(cors());
+      this.server.options("*", cors());
+    
+      next();
+    });
   }
 }
 
