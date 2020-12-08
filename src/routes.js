a66847bcbef10af357058a077feecb6979f9b252
Thiago9220_github <Thiago.ramos356@gmail.com>
2020-12-07 21:03:42 -0300

    alooraa

--- src/routes.js
+++ src/routes.js
@@ -10,25 +10,25 @@ routes.get('/', function (req,res){
 })
 
 // CRUD Funcionario
-routes.get('/funcionario/:id', AdminController.show);
-routes.post('/funcionario', AdminController.store);
+routes.get('funcionario/:id', AdminController.show);
+routes.post('funcionario', AdminController.store);
 
-routes.put('/funcionario/:id', AdminController.update);
-routes.delete('/funcionario/:id', AdminController.delete);
+routes.put('funcionario/:id', AdminController.update);
+routes.delete('funcionario/:id', AdminController.delete);
 
 // CRUD Produto
-routes.get('/produto/:id', ProdutoController.show);
-routes.post('/produto', ProdutoController.store);
+routes.get('produto/:id', ProdutoController.show);
+routes.post('produto', ProdutoController.store);
 
-routes.put('/produto/:id', ProdutoController.update);
-routes.delete('/produto/:id', ProdutoController.delete);
+routes.put('produto/:id', ProdutoController.update);
+routes.delete('produto/:id', ProdutoController.delete);
 
 // CRUD Cliente
-routes.get('/cliente/:id', ClienteController.show);
-routes.post('/cliente', ClienteController.store);
+routes.get('cliente/:id', ClienteController.show);
+routes.post('cliente', ClienteController.store);
 
-routes.put('/cliente/:id', ClienteController.update);
-routes.delete('/cliente/:id', ClienteController.delete);
+routes.put('cliente/:id', ClienteController.update);
+routes.delete('cliente/:id', ClienteController.delete);
 
 
 module.exports = routes;
